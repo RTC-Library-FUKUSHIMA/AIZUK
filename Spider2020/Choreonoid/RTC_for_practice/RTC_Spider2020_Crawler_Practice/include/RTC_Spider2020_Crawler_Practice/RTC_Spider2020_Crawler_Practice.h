2b7a6f52358ac46c40664361ecf84f942c10c269
Chiaki Saito <Saito.chiaki@aizuk.jp>
2020-04-06 10:04:16 +0900

    add RTC_Spider2020_Crawler_Practice to control the crawler of Spider2020 model

---
+++ Spider2020/Choreonoid/RTC_for_practice/RTC_Spider2020_Crawler_Practice/include/RTC_Spider2020_Crawler_Practice/RTC_Spider2020_Crawler_Practice.h
@@ -0,0 +1,471 @@
+﻿// -*- C++ -*-
+/*!
+ * @file  RTC_Spider2020_Crawler_Practice.h
+ * @brief Spider2020 コレオノイドモデルの台車制御RTC
+ * @date  $Date$
+ *
+ * $Id$
+ */
+
+#ifndef RTC_SPIDER2020_CRAWLER_PRACTICE_H
+#define RTC_SPIDER2020_CRAWLER_PRACTICE_H
+
+#include <cnoid/BodyIoRTC>
+#include <cnoid/EigenUtil>
+#include <rtm/idl/BasicDataTypeSkel.h>
+#include <rtm/idl/ExtendedDataTypesSkel.h>
+#include <rtm/idl/InterfaceDataTypesSkel.h>
+
+// Service implementation headers
+// <rtc-template block="service_impl_h">
+
+// </rtc-template>
+
+// Service Consumer stub headers
+// <rtc-template block="consumer_stub_h">
+#include "ExtendedDataTypesStub.h"
+#include "BasicDataTypeStub.h"
+
+// </rtc-template>
+
+#include <rtm/Manager.h>
+#include <rtm/DataFlowComponentBase.h>
+#include <rtm/CorbaPort.h>
+#include <rtm/DataInPort.h>
+#include <rtm/DataOutPort.h>
+
+#include "FlipperPwmWrapper.h"
+
+using namespace cnoid;
+
+/*!
+ * @enum 列挙子
+ * リンクデータのインデクサ
+ */
+enum LINK_t
+{
+	LINK_CHASSIS,					/*!< シャーシ						*/
+	LINK_FR_FLIPPER,				/*!< 右前方フリッパー				*/
+	LINK_FL_FLIPPER,				/*!< 左前方フリッパー				*/
+	LINK_BR_FLIPPER,				/*!< 右後方フリッパー				*/
+	LINK_BL_FLIPPER,				/*!< 左後方フリッパー				*/
+	LINK_R_BELT,					/*!< 右メインベルト					*/
+	LINK_L_BELT,					/*!< 左メインベルト					*/
+	LINK_FR_SUB_BELT,				/*!< 右前方サブベルト				*/
+	LINK_FL_SUB_BELT,				/*!< 左前方サブベルト				*/
+	LINK_BR_SUB_BELT,				/*!< 右後方サブベルト				*/
+	LINK_BL_SUB_BELT,				/*!< 左後方サブベルト				*/
+	LINK_R_BELT_SPROCKET,			/*!< 右メインベルトスプロケット		*/
+	LINK_L_BELT_SPROCKET,			/*!< 左メインベルトスプロケット		*/
+	LINK_FR_SUB_BELT_SPROCKET,		/*!< 右前方サブベルトスプロケット	*/
+	LINK_FL_SUB_BELT_SPROCKET,		/*!< 左前方サブベルトスプロケット	*/
+	LINK_BR_SUB_BELT_SPROCKET,		/*!< 右後方サブベルトスプロケット	*/
+	LINK_BL_SUB_BELT_SPROCKET,		/*!< 左後方サブベルトスプロケット	*/
+	LINK_NUM,						/*!< リンクデータの数				*/
+};
+
+/*!
+ * @enum 列挙子
+ * 関節データのインデクサ
+ */
+enum JOINT_t
+{
+	JOINT_FR_FLIPPER,	/*!< 右前方フリッパー	*/
+	JOINT_FL_FLIPPER,	/*!< 左前方フリッパー	*/
+	JOINT_BR_FLIPPER,	/*!< 右後方フリッパー	*/
+	JOINT_BL_FLIPPER,	/*!< 左後方フリッパー	*/
+	JOINT_NUM,			/*!< 関節データの数		*/
+};
+
+/*!
+ * @class RTC_Spider2020_Crawler_Practice
+ * @brief Spider2020 コレオノイドモデルの台車制御RTC
+ *
+ */
+class RTC_Spider2020_Crawler_Practice
+  : public BodyIoRTC
+{
+public:
+	/*!
+	 * @brief constructor
+	 * @param manager Maneger Object
+	 */
+	RTC_Spider2020_Crawler_Practice(RTC::Manager* manager);
+
+	/*!
+	 * @brief destructor
+	 */
+	~RTC_Spider2020_Crawler_Practice();
+
+	// <rtc-template block="public_attribute">
+
+	// </rtc-template>
+
+	// <rtc-template block="public_operation">
+
+	// </rtc-template>
+
+	/***
+	 *
+	 * The initialize action (on CREATED->ALIVE transition)
+	 *
+	 * @return RTC::ReturnCode_t
+	 *
+	 *
+	 */
+	virtual RTC::ReturnCode_t onInitialize(Body* body) override;
+
+	/***
+	 *
+	 * The finalize action (on ALIVE->END transition)
+	 *
+	 * @return RTC::ReturnCode_t
+	 *
+	 *
+	 */
+	// virtual RTC::ReturnCode_t onFinalize();
+
+	/***
+	 *
+	 * The startup action when ExecutionContext startup
+	 *
+	 * @param ec_id target ExecutionContext Id
+	 *
+	 * @return RTC::ReturnCode_t
+	 *
+	 *
+	 */
+	// virtual RTC::ReturnCode_t onStartup(RTC::UniqueId ec_id);
+
+	/***
+	 *
+	 * The shutdown action when ExecutionContext stop
+	 *
+	 * @param ec_id target ExecutionContext Id
+	 *
+	 * @return RTC::ReturnCode_t
+	 *
+	 *
+	 */
+	// virtual RTC::ReturnCode_t onShutdown(RTC::UniqueId ec_id);
+
+	/***
+	 *
+	 * The activated action (Active state entry action)
+	 *
+	 * @param ec_id target ExecutionContext Id
+	 *
+	 * @return RTC::ReturnCode_t
+	 *
+	 *
+	 */
+	virtual RTC::ReturnCode_t onActivated(RTC::UniqueId ec_id);
+
+	/***
+	 *
+	 * The deactivated action (Active state exit action)
+	 *
+	 * @param ec_id target ExecutionContext Id
+	 *
+	 * @return RTC::ReturnCode_t
+	 *
+	 *
+	 */
+	// virtual RTC::ReturnCode_t onDeactivated(RTC::UniqueId ec_id);
+
+	/***
+	 *
+	 * The execution action that is invoked periodically
+	 *
+	 * @param ec_id target ExecutionContext Id
+	 *
+	 * @return RTC::ReturnCode_t
+	 *
+	 *
+	 */
+	// virtual RTC::ReturnCode_t onExecute(RTC::UniqueId ec_id);
+
+	/***
+	 *
+	 * The aborting action when main logic error occurred.
+	 *
+	 * @param ec_id target ExecutionContext Id
+	 *
+	 * @return RTC::ReturnCode_t
+	 *
+	 *
+	 */
+	// virtual RTC::ReturnCode_t onAborting(RTC::UniqueId ec_id);
+
+	/***
+	 *
+	 * The error action in ERROR state
+	 *
+	 * @param ec_id target ExecutionContext Id
+	 *
+	 * @return RTC::ReturnCode_t
+	 *
+	 *
+	 */
+	// virtual RTC::ReturnCode_t onError(RTC::UniqueId ec_id);
+
+	/***
+	 *
+	 * The reset action that is invoked resetting
+	 *
+	 * @param ec_id target ExecutionContext Id
+	 *
+	 * @return RTC::ReturnCode_t
+	 *
+	 *
+	 */
+	// virtual RTC::ReturnCode_t onReset(RTC::UniqueId ec_id);
+
+	/***
+	 *
+	 * The state update action that is invoked after onExecute() action
+	 *
+	 * @param ec_id target ExecutionContext Id
+	 *
+	 * @return RTC::ReturnCode_t
+	 *
+	 *
+	 */
+	// virtual RTC::ReturnCode_t onStateUpdate(RTC::UniqueId ec_id);
+
+	/***
+	 *
+	 * The action that is invoked when execution context's rate is changed
+	 *
+	 * @param ec_id target ExecutionContext Id
+	 *
+	 * @return RTC::ReturnCode_t
+	 *
+	 *
+	 */
+	// virtual RTC::ReturnCode_t onRateChanged(RTC::UniqueId ec_id);
+
+	/*!
+	 * シミュレーションの開始処理
+	 * @method initializeSimulation
+	 * @param io IOオブジェクトのポインタ
+	 * @retval true 正常終了
+	 * @retval false 異常終了
+	 */
+	virtual bool initializeSimulation(ControllerIO* io) override;
+
+	/*!
+	 * シミュレーションの出力ポートへの書込処理
+	 * @method inputFromSimulator
+	 * @return なし
+	 */
+	virtual void inputFromSimulator() override;
+
+	/*!
+	 * シミュレーションの入力ポートからの読込処理
+	 * @method outputToSimulator
+	 * @return なし
+	 */
+	virtual void outputToSimulator() override;
+
+	/*!
+	 * シミュレーションの終了処理
+	 * @method stopSimulation
+	 * @return なし
+	 */
+	virtual void stopSimulation() override;
+
+	/*!
+	 * ２次元の速度をポートから読み込む
+	 * @method ReadVelocity2D
+	 * @return なし
+	 */
+	void ReadVelocity2D(void);
+
+	/*!
+	 * ２次元の速度をリンクに書き込む
+	 * @method WriteVelocity2D
+	 * @return なし
+	 */
+	void WriteVelocity2D(void);
+
+	/*!
+	 * フリッパーのPWM指令をポートから読み込む
+	 * @method ReadFlipperPwm
+	 * @return なし
+	 */
+	void ReadFlipperPwm(void);
+
+	/*!
+	 * フリッパーのPWM指令をリンクに書き込む
+	 * @method WriteFlipperPwm
+	 * @return なし
+	 */
+	void WriteFlipperPwm(void);
+
+	/*!
+	 * PWM指令からモータの回転数を求め、目標角度を対象にトルクでPD制御する
+	 * @method FlipperPwm
+	 * @param iJointIndex 関節データのインデクサ
+	 * @param lPwm PWM指令
+	 */
+	void FlipperPwm(int iJointIndex, long lPwm);
+
+protected:
+	// <rtc-template block="protected_attribute">
+
+	// </rtc-template>
+
+	// <rtc-template block="protected_operation">
+
+	// </rtc-template>
+
+	// Configuration variable declaration
+	// <rtc-template block="config_declare">
+	/*!
+	 * Wheel Radius
+	 * - Name: メインクローラの車輪半径 WHEEL_RADIUS
+	 * - DefaultValue: 0.0769
+	 * - Unit: [m]
+	 */
+	double m_WHEEL_RADIUS;
+	/*!
+	 * Turning Radius
+	 * - Name: 旋回半径 TURNING_RADIUS
+	 * - DefaultValue: 0.0938
+	 * - Unit: [m]
+	 */
+	double m_TURNING_RADIUS;
+	/*!
+	 * メインモータの最大連続トルク
+	 * - Name: トルク MAIN_MOTOR_TORQUE
+	 * - DefaultValue: 405e-3
+	 * - Unit: [Nm]
+	 */
+	double m_MAIN_MOTOR_TORQUE;
+	/*!
+	 * メインモータの無負荷回転数
+	 * - Name: 回転数 MAIN_MOTOR_RPM
+	 * - DefaultValue: 5950
+	 * - Unit: [rpm]
+	 */
+	long int m_MAIN_MOTOR_RPM;
+	/*!
+	 * メインモータのトルク勾配
+	 * - Name: トルク勾配 MAIN_MOTOR_GRADIENT
+	 * - DefaultValue: 0.668e+3
+	 * - Unit: [rpm/Nm]
+	 */
+	double m_MAIN_MOTOR_GRADIENT;
+	/*!
+	 * メインモータの減速比
+	 * - Name: 減速比 MAIN_MOTOR_GEAR_RATIO
+	 * - DefaultValue: 243
+	 */
+	double m_MAIN_MOTOR_GEAR_RATIO;
+	/*!
+	 * サブモータの最大連続トルク
+	 * - Name: トルク SUB_MOTOR_TORQUE
+	 * - DefaultValue: 105e-3
+	 * - Unit: [Nm]
+	 */
+	double m_SUB_MOTOR_TORQUE;
+	/*!
+	 * サブモータの無負荷回転数
+	 * - Name: 回転数 SUB_MOTOR_RPM
+	 * - DefaultValue: 7750
+	 * - Unit: [rpm]
+	 */
+	long int m_SUB_MOTOR_RPM;
+	/*!
+	 * サブモータのトルク勾配
+	 * - Name: トルク勾配 SUB_MOTOR_GRADIENT
+	 * - DefaultValue: 6.54e+3
+	 * - Unit: [rpm/Nm]
+	 */
+	double m_SUB_MOTOR_GRADIENT;
+	/*!
+	 * サブモータの減速比
+	 * - Name: 減速比 SUB_MOTOR_GEAR_RATIO
+	 * - DefaultValue: 1080
+	 */
+	double m_SUB_MOTOR_GEAR_RATIO;
+	/*!
+	 * 入力ポートへ指定時間以上新しい入力がなければ、
+	 * 暴走を回避するため入力データのクリアなどを実施する。
+	 * - Name: フェイルセーフ時間 FAILSAFE_SEC
+	 * - DefaultValue: 1.0
+	 */
+	double m_FAILSAFE_SEC;
+
+	// </rtc-template>
+
+	// DataInPort declaration
+	// <rtc-template block="inport_declare">
+	RTC::TimedVelocity2D m_Velocity2D_In;
+	/*!
+	 * ２次元の速度
+	 * - Type: TimedVelocity2D
+	 */
+	RTC::InPort<RTC::TimedVelocity2D> m_Velocity2D_InIn;
+	RTC::TimedLongSeq m_FlipperPwm_In;
+	/*!
+	 * フリッパーのPWM指令
+	 * - Type: TimedLongSeq
+	 * - Number: 4
+	 */
+	RTC::InPort<RTC::TimedLongSeq> m_FlipperPwm_InIn;
+
+	// </rtc-template>
+
+
+	// DataOutPort declaration
+	// <rtc-template block="outport_declare">
+
+	// </rtc-template>
+
+	// CORBA Port declaration
+	// <rtc-template block="corbaport_declare">
+
+	// </rtc-template>
+
+	// Service declaration
+	// <rtc-template block="service_declare">
+
+	// </rtc-template>
+
+	// Consumer declaration
+	// <rtc-template block="consumer_declare">
+
+	// </rtc-template>
+
+private:
+	// <rtc-template block="private_attribute">
+
+	// </rtc-template>
+
+	// <rtc-template block="private_operation">
+
+	// </rtc-template>
+
+	FlipperPwmWrapper m_FlipperPwm_InWrap;			/*!< m_FlipperPwm_Inのラッパークラス	*/
+
+	Link*		m_cnoid_Link[LINK_NUM];				/*!< コレオノイドモデルのリンク			*/
+	double		m_cnoid_RefAngle[JOINT_NUM];		/*!< コレオノイドモデルの目標関節角度	*/
+	double		m_cnoid_MotorAngle[JOINT_NUM];		/*!< サブモータの角度					*/
+	double		m_cnoid_TimeStep;					/*!< シミュレーションの周期				*/
+
+	bool		m_bPseudoContinuousTrack;			/*!< 無限軌道の判別フラグ				*/
+
+	struct timeval m_stVelocity2DReadTime;			/*!< m_Velocity2D_Inの入力時刻			*/
+	struct timeval m_stFlipperPwmReadTime;			/*!< m_FlipperPwm_Inの入力時刻			*/
+
+};
+
+
+extern "C"
+{
+	DLL_EXPORT void RTC_Spider2020_Crawler_PracticeInit(RTC::Manager* manager);
+};
+
+#endif // RTC_SPIDER2020_CRAWLER_PRACTICE_H
---
+++ Spider2020/Choreonoid/RTC_for_practice/RTC_Spider2020_Crawler_Practice/src/RTC_Spider2020_Crawler_Practice.cpp
@@ -0,0 +1,588 @@
+﻿// -*- C++ -*-
+/*!
+ * @file  RTC_Spider2020_Crawler_Practice.cpp
+ * @brief Spider2020 コレオノイドモデルの台車制御RTC
+ * @date $Date$
+ *
+ * $Id$
+ */
+
+#include "RTC_Spider2020_Crawler_Practice.h"
+
+#define VELOCITY_MAX		(0.15)	/*!< クローラ最大速度		*/
+#define FLIPPER_PWM_MIN		(10)	/*!< PWM指令最小値			*/
+#define FLIPPER_PWM_MAX		(90)	/*!< PWM指令最大値			*/
+
+// timeval構造体の差を計算する(x1-x2)
+#define TIMEVAL_DIFF(x1,x2)	(((double)x1.tv_sec + (double)x1.tv_usec/1000000.0) - ((double)x2.tv_sec + (double)x2.tv_usec/1000000.0))
+
+// Module specification
+// <rtc-template block="module_spec">
+static const char* rtc_spider2020_crawler_practice_spec[] =
+{
+	"implementation_id", "RTC_Spider2020_Crawler_Practice",
+	"type_name",         "RTC_Spider2020_Crawler_Practice",
+	"description",       "Spider2020 コレオノイドモデルの台車制御RTC",
+	"version",           "1.0.0",
+	"vendor",            "AIZUK",
+	"category",          "Controller",
+	"activity_type",     "PERIODIC",
+	"kind",              "DataFlowComponent",
+	"max_instance",      "1",
+	"language",          "C++",
+	"lang_type",         "compile",
+
+	// Configuration variables
+	"conf.default.WHEEL_RADIUS", "0.0769",
+	"conf.default.TURNING_RADIUS", "0.0938",
+	"conf.default.MAIN_MOTOR_TORQUE", "405e-3",
+	"conf.default.MAIN_MOTOR_RPM", "5950",
+	"conf.default.MAIN_MOTOR_GRADIENT", "0.668e+3",
+	"conf.default.MAIN_MOTOR_GEAR_RATIO", "243",
+	"conf.default.SUB_MOTOR_TORQUE", "105e-3",
+	"conf.default.SUB_MOTOR_RPM", "7750",
+	"conf.default.SUB_MOTOR_GRADIENT", "6.54e+3",
+	"conf.default.SUB_MOTOR_GEAR_RATIO", "1080",
+	"conf.default.FAILSAFE_SEC", "1.0",
+
+	// Widget
+	"conf.__widget__.WHEEL_RADIUS", "text",
+	"conf.__widget__.TURNING_RADIUS", "text",
+	"conf.__widget__.MAIN_MOTOR_TORQUE", "text",
+	"conf.__widget__.MAIN_MOTOR_RPM", "text",
+	"conf.__widget__.MAIN_MOTOR_GRADIENT", "text",
+	"conf.__widget__.MAIN_MOTOR_GEAR_RATIO", "text",
+	"conf.__widget__.SUB_MOTOR_TORQUE", "text",
+	"conf.__widget__.SUB_MOTOR_RPM", "text",
+	"conf.__widget__.SUB_MOTOR_GRADIENT", "text",
+	"conf.__widget__.SUB_MOTOR_GEAR_RATIO", "text",
+	"conf.__widget__.FAILSAFE_SEC", "text",
+
+	// Constraints
+	"conf.__type__.WHEEL_RADIUS", "double",
+	"conf.__type__.TURNING_RADIUS", "double",
+	"conf.__type__.MAIN_MOTOR_TORQUE", "double",
+	"conf.__type__.MAIN_MOTOR_RPM", "long",
+	"conf.__type__.MAIN_MOTOR_GRADIENT", "double",
+	"conf.__type__.MAIN_MOTOR_GEAR_RATIO", "double",
+	"conf.__type__.SUB_MOTOR_TORQUE", "double",
+	"conf.__type__.SUB_MOTOR_RPM", "long",
+	"conf.__type__.SUB_MOTOR_GRADIENT", "double",
+	"conf.__type__.SUB_MOTOR_GEAR_RATIO", "double",
+	"conf.__type__.FAILSAFE_SEC", "double",
+
+	""
+};
+// </rtc-template>
+
+/*!
+ * @brief constructor
+ * @param manager Maneger Object
+ */
+RTC_Spider2020_Crawler_Practice::RTC_Spider2020_Crawler_Practice(RTC::Manager* manager)
+	// <rtc-template block="initializer">
+	: BodyIoRTC(manager)
+	, m_Velocity2D_InIn("Velocity2D_In", m_Velocity2D_In)
+	, m_FlipperPwm_InIn("FlipperPwm_In", m_FlipperPwm_In)
+
+	// </rtc-template>
+	, m_FlipperPwm_InWrap(m_FlipperPwm_In)
+	, m_stVelocity2DReadTime({0})
+	, m_stFlipperPwmReadTime({0})
+{
+}
+
+/*!
+ * @brief destructor
+ */
+RTC_Spider2020_Crawler_Practice::~RTC_Spider2020_Crawler_Practice()
+{
+}
+
+RTC::ReturnCode_t RTC_Spider2020_Crawler_Practice::onInitialize(Body* body)
+{
+	// Registration: InPort/OutPort/Service
+	// <rtc-template block="registration">
+	// Set InPort buffers
+	addInPort("Velocity2D_In", m_Velocity2D_InIn);
+	addInPort("FlipperPwm_In", m_FlipperPwm_InIn);
+
+	// Set OutPort buffer
+
+	// Set service provider to Ports
+
+	// Set service consumers to Ports
+
+	// Set CORBA Service Ports
+
+	// </rtc-template>
+
+	// <rtc-template block="bind_config">
+	// Bind variables and configuration variable
+	bindParameter("WHEEL_RADIUS", m_WHEEL_RADIUS, "0.0769");
+	bindParameter("TURNING_RADIUS", m_TURNING_RADIUS, "0.0938");
+	bindParameter("MAIN_MOTOR_TORQUE", m_MAIN_MOTOR_TORQUE, "405e-3");
+	bindParameter("MAIN_MOTOR_RPM", m_MAIN_MOTOR_RPM, "5950");
+	bindParameter("MAIN_MOTOR_GRADIENT", m_MAIN_MOTOR_GRADIENT, "0.668e+3");
+	bindParameter("MAIN_MOTOR_GEAR_RATIO", m_MAIN_MOTOR_GEAR_RATIO, "243");
+	bindParameter("SUB_MOTOR_TORQUE", m_SUB_MOTOR_TORQUE, "105e-3");
+	bindParameter("SUB_MOTOR_RPM", m_SUB_MOTOR_RPM, "7750");
+	bindParameter("SUB_MOTOR_GRADIENT", m_SUB_MOTOR_GRADIENT, "6.54e+3");
+	bindParameter("SUB_MOTOR_GEAR_RATIO", m_SUB_MOTOR_GEAR_RATIO, "1080");
+	bindParameter("FAILSAFE_SEC", m_FAILSAFE_SEC, "1.0");
+	// </rtc-template>
+
+	// ２次元の速度を初期化する
+	m_Velocity2D_In.data.vx = 0.0;
+	m_Velocity2D_In.data.vy = 0.0;
+	m_Velocity2D_In.data.va = 0.0;
+
+	// フリッパーのPWM指令を初期化する
+	m_FlipperPwm_InWrap.InitData();
+
+	return RTC::RTC_OK;
+}
+
+/*
+RTC::ReturnCode_t RTC_Spider2020_Crawler_Practice::onFinalize()
+{
+	return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t RTC_Spider2020_Crawler_Practice::onStartup(RTC::UniqueId ec_id)
+{
+	return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t RTC_Spider2020_Crawler_Practice::onShutdown(RTC::UniqueId ec_id)
+{
+	return RTC::RTC_OK;
+}
+*/
+
+RTC::ReturnCode_t RTC_Spider2020_Crawler_Practice::onActivated(RTC::UniqueId ec_id)
+{
+	return RTC::RTC_OK;
+}
+
+/*
+RTC::ReturnCode_t RTC_Spider2020_Crawler_Practice::onDeactivated(RTC::UniqueId ec_id)
+{
+	return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t RTC_Spider2020_Crawler_Practice::onExecute(RTC::UniqueId ec_id)
+{
+	return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t RTC_Spider2020_Crawler_Practice::onAborting(RTC::UniqueId ec_id)
+{
+	return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t RTC_Spider2020_Crawler_Practice::onError(RTC::UniqueId ec_id)
+{
+	return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t RTC_Spider2020_Crawler_Practice::onReset(RTC::UniqueId ec_id)
+{
+	return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t RTC_Spider2020_Crawler_Practice::onStateUpdate(RTC::UniqueId ec_id)
+{
+	return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t RTC_Spider2020_Crawler_Practice::onRateChanged(RTC::UniqueId ec_id)
+{
+	return RTC::RTC_OK;
+}
+*/
+
+bool RTC_Spider2020_Crawler_Practice::initializeSimulation(ControllerIO* io)
+{
+	int i = 0;
+
+	if(io != nullptr)
+	{
+		Body* body = io->body();
+		if(body != nullptr)
+		{
+			// リンクを取得する
+			m_cnoid_Link[LINK_CHASSIS]			= body->link("CHASSIS");
+			m_cnoid_Link[LINK_FR_FLIPPER]		= body->link("FR_FLIPPER");
+			m_cnoid_Link[LINK_FL_FLIPPER]		= body->link("FL_FLIPPER");
+			m_cnoid_Link[LINK_BR_FLIPPER]		= body->link("BR_FLIPPER");
+			m_cnoid_Link[LINK_BL_FLIPPER]		= body->link("BL_FLIPPER");
+			m_cnoid_Link[LINK_R_BELT]			= body->link("R_BELT");
+			m_cnoid_Link[LINK_L_BELT]			= body->link("L_BELT");
+			m_cnoid_Link[LINK_FR_SUB_BELT]		= body->link("FR_SUB_BELT");
+			m_cnoid_Link[LINK_FL_SUB_BELT]		= body->link("FL_SUB_BELT");
+			m_cnoid_Link[LINK_BR_SUB_BELT]		= body->link("BR_SUB_BELT");
+			m_cnoid_Link[LINK_BL_SUB_BELT]		= body->link("BL_SUB_BELT");
+			for(i = 0; i <= LINK_BL_FLIPPER; i++)
+			{
+				if(m_cnoid_Link[i] == nullptr)
+				{
+					return false;
+				}
+			}
+
+			// 走行モードを判断する
+			if((m_cnoid_Link[LINK_R_BELT] != nullptr) && (m_cnoid_Link[LINK_L_BELT] != nullptr))
+			{
+				// 無限軌道
+				m_bPseudoContinuousTrack = true;
+
+				for(i = LINK_R_BELT_SPROCKET; i <= LINK_BL_SUB_BELT_SPROCKET; i++)
+				{
+					m_cnoid_Link[i] = nullptr;
+				}
+			}
+			else
+			{
+				// AGXクローラ
+				m_bPseudoContinuousTrack = false;
+
+				m_cnoid_Link[LINK_R_BELT_SPROCKET]			= body->link("R_BELT_SPROCKET");
+				m_cnoid_Link[LINK_L_BELT_SPROCKET]			= body->link("L_BELT_SPROCKET");
+				m_cnoid_Link[LINK_FR_SUB_BELT_SPROCKET]		= body->link("FR_SUB_BELT_SPROCKET");
+				m_cnoid_Link[LINK_FL_SUB_BELT_SPROCKET]		= body->link("FL_SUB_BELT_SPROCKET");
+				m_cnoid_Link[LINK_BR_SUB_BELT_SPROCKET]		= body->link("BR_SUB_BELT_SPROCKET");
+				m_cnoid_Link[LINK_BL_SUB_BELT_SPROCKET]		= body->link("BL_SUB_BELT_SPROCKET");
+				for(i = LINK_R_BELT_SPROCKET; i <= LINK_BL_SUB_BELT_SPROCKET; i++)
+				{
+					if(m_cnoid_Link[i] == nullptr)
+					{
+						return false;
+					}
+				}
+			}
+
+			// 作動モードを設定する
+			m_cnoid_Link[LINK_FR_FLIPPER]->setActuationMode(Link::JOINT_TORQUE);
+			m_cnoid_Link[LINK_FL_FLIPPER]->setActuationMode(Link::JOINT_TORQUE);
+			m_cnoid_Link[LINK_BR_FLIPPER]->setActuationMode(Link::JOINT_TORQUE);
+			m_cnoid_Link[LINK_BL_FLIPPER]->setActuationMode(Link::JOINT_TORQUE);
+			if(m_bPseudoContinuousTrack)
+			{
+				m_cnoid_Link[LINK_R_BELT]->setActuationMode(Link::JOINT_SURFACE_VELOCITY);
+				m_cnoid_Link[LINK_L_BELT]->setActuationMode(Link::JOINT_SURFACE_VELOCITY);
+				m_cnoid_Link[LINK_FR_SUB_BELT]->setActuationMode(Link::JOINT_SURFACE_VELOCITY);
+				m_cnoid_Link[LINK_FL_SUB_BELT]->setActuationMode(Link::JOINT_SURFACE_VELOCITY);
+				m_cnoid_Link[LINK_BR_SUB_BELT]->setActuationMode(Link::JOINT_SURFACE_VELOCITY);
+				m_cnoid_Link[LINK_BL_SUB_BELT]->setActuationMode(Link::JOINT_SURFACE_VELOCITY);
+			}
+			else
+			{
+				m_cnoid_Link[LINK_R_BELT_SPROCKET]->setActuationMode(Link::JOINT_VELOCITY);
+				m_cnoid_Link[LINK_L_BELT_SPROCKET]->setActuationMode(Link::JOINT_VELOCITY);
+				m_cnoid_Link[LINK_FR_SUB_BELT_SPROCKET]->setActuationMode(Link::JOINT_VELOCITY);
+				m_cnoid_Link[LINK_FL_SUB_BELT_SPROCKET]->setActuationMode(Link::JOINT_VELOCITY);
+				m_cnoid_Link[LINK_BR_SUB_BELT_SPROCKET]->setActuationMode(Link::JOINT_VELOCITY);
+				m_cnoid_Link[LINK_BL_SUB_BELT_SPROCKET]->setActuationMode(Link::JOINT_VELOCITY);
+			}
+		}
+
+		// 実行周期を取得する
+		m_cnoid_TimeStep = io->timeStep();
+	}
+
+	return true;
+}
+
+void RTC_Spider2020_Crawler_Practice::inputFromSimulator()
+{
+}
+
+void RTC_Spider2020_Crawler_Practice::outputToSimulator()
+{
+	// ２次元の速度をポートから読み込む
+	ReadVelocity2D();
+
+	// ２次元の速度をリンクに書き込む
+	WriteVelocity2D();
+
+	// フリッパーのPWM指令をポートから読み込む
+	ReadFlipperPwm();
+
+	// フリッパーのPWM指令をリンクに書き込む
+	WriteFlipperPwm();
+}
+
+void RTC_Spider2020_Crawler_Practice::stopSimulation()
+{
+}
+
+void RTC_Spider2020_Crawler_Practice::ReadVelocity2D(void)
+{
+	struct timeval ti = {0};
+
+	// 現在の時刻を取得する
+	gettimeofday(&ti, NULL);
+
+	// ２次元の速度を処理する
+	if(m_Velocity2D_InIn.isNew())
+	{
+		// 読み込み時刻を更新する
+		m_stVelocity2DReadTime = ti;
+
+		// 次のデータを読み込む
+		m_Velocity2D_InIn.read();
+	}
+	else
+	{
+		if(m_FAILSAFE_SEC <= TIMEVAL_DIFF(ti,m_stVelocity2DReadTime))
+		{
+			// ２次元の速度の入力を初期化する(暴走回避)
+			m_Velocity2D_In.data.vx = 0.0;
+			m_Velocity2D_In.data.vy = 0.0;
+			m_Velocity2D_In.data.va = 0.0;
+		}
+	}
+}
+
+void RTC_Spider2020_Crawler_Practice::WriteVelocity2D(void)
+{
+	double dVelocityR = 0.0;
+	double dVelocityL = 0.0;
+
+	// 右メインベルトの速度
+	dVelocityR = (double)m_Velocity2D_In.data.vx + (m_Velocity2D_In.data.va*m_TURNING_RADIUS)/2.0;
+	if(VELOCITY_MAX < fabs(dVelocityR))
+	{
+		if(0.0 < dVelocityR)
+		{
+			dVelocityR = VELOCITY_MAX;
+		}
+		else
+		{
+			dVelocityR = -VELOCITY_MAX;
+		}
+	}
+
+	// 左メインベルトの速度
+	dVelocityL = (double)m_Velocity2D_In.data.vx - (m_Velocity2D_In.data.va*m_TURNING_RADIUS)/2.0;
+	if(VELOCITY_MAX < fabs(dVelocityL))
+	{
+		if(0.0 < dVelocityL)
+		{
+			dVelocityL = VELOCITY_MAX;
+		}
+		else
+		{
+			dVelocityL = -VELOCITY_MAX;
+		}
+	}
+
+	if(m_bPseudoContinuousTrack)
+	{
+		m_cnoid_Link[LINK_R_BELT]->dq_target() = dVelocityR;
+		m_cnoid_Link[LINK_L_BELT]->dq_target() = dVelocityL;
+		m_cnoid_Link[LINK_FR_SUB_BELT]->dq_target() = dVelocityR;
+		m_cnoid_Link[LINK_FL_SUB_BELT]->dq_target() = dVelocityL;
+		m_cnoid_Link[LINK_BR_SUB_BELT]->dq_target() = dVelocityR;
+		m_cnoid_Link[LINK_BL_SUB_BELT]->dq_target() = dVelocityL;
+	}
+	else
+	{
+		m_cnoid_Link[LINK_R_BELT_SPROCKET]->dq_target() = dVelocityR/m_WHEEL_RADIUS;
+		m_cnoid_Link[LINK_L_BELT_SPROCKET]->dq_target() = dVelocityL/m_WHEEL_RADIUS;
+		m_cnoid_Link[LINK_FR_SUB_BELT_SPROCKET]->dq_target() = dVelocityR/m_WHEEL_RADIUS;
+		m_cnoid_Link[LINK_FL_SUB_BELT_SPROCKET]->dq_target() = dVelocityL/m_WHEEL_RADIUS;
+		m_cnoid_Link[LINK_BR_SUB_BELT_SPROCKET]->dq_target() = dVelocityR/m_WHEEL_RADIUS;
+		m_cnoid_Link[LINK_BL_SUB_BELT_SPROCKET]->dq_target() = dVelocityL/m_WHEEL_RADIUS;
+	}
+}
+
+void RTC_Spider2020_Crawler_Practice::ReadFlipperPwm(void)
+{
+	struct timeval ti = {0};
+
+	// 現在の時刻を取得する
+	gettimeofday(&ti, NULL);
+
+	// フリッパーのPWM指令を処理する
+	if(m_FlipperPwm_InIn.isNew())
+	{
+		// 読み込み時刻を更新する
+		m_stFlipperPwmReadTime = ti;
+
+		// 次のデータを読み込む
+		m_FlipperPwm_InIn.read();
+	}
+	else
+	{
+		if(m_FAILSAFE_SEC <= TIMEVAL_DIFF(ti,m_stFlipperPwmReadTime))
+		{
+			// フリッパーのPWM指令を初期化する(暴走回避)
+			m_FlipperPwm_InWrap.InitData();
+		}
+	}
+}
+
+void RTC_Spider2020_Crawler_Practice::WriteFlipperPwm(void)
+{
+	long lFlipperPwmFR = 0;
+	long lFlipperPwmFL = 0;
+	long lFlipperPwmBR = 0;
+	long lFlipperPwmBL = 0;
+
+	// 右前方フリッパーのPWM
+	lFlipperPwmFR = m_FlipperPwm_InWrap.GetFrontRightPwm();
+	if(FLIPPER_PWM_MAX < labs(lFlipperPwmFR))
+	{
+		if(0 < lFlipperPwmFR)
+		{
+			lFlipperPwmFR = FLIPPER_PWM_MAX;
+		}
+		else
+		{
+			lFlipperPwmFR = -FLIPPER_PWM_MAX;
+		}
+	}
+
+	// 左前方フリッパーのPWM
+	lFlipperPwmFL = m_FlipperPwm_InWrap.GetFrontLeftPwm();
+	if(FLIPPER_PWM_MAX < labs(lFlipperPwmFL))
+	{
+		if(0 < lFlipperPwmFL)
+		{
+			lFlipperPwmFL = FLIPPER_PWM_MAX;
+		}
+		else
+		{
+			lFlipperPwmFL = -FLIPPER_PWM_MAX;
+		}
+	}
+
+	// 右後方フリッパーのPWM
+	lFlipperPwmBR = m_FlipperPwm_InWrap.GetBackRightPwm();
+	if(FLIPPER_PWM_MAX < labs(lFlipperPwmBR))
+	{
+		if(0 < lFlipperPwmBR)
+		{
+			lFlipperPwmBR = FLIPPER_PWM_MAX;
+		}
+		else
+		{
+			lFlipperPwmBR = -FLIPPER_PWM_MAX;
+		}
+	}
+
+	// 左後方フリッパーのPWM
+	lFlipperPwmBL = m_FlipperPwm_InWrap.GetBackLeftPwm();
+	if(FLIPPER_PWM_MAX < labs(lFlipperPwmBL))
+	{
+		if(0 < lFlipperPwmBL)
+		{
+			lFlipperPwmBL = FLIPPER_PWM_MAX;
+		}
+		else
+		{
+			lFlipperPwmBL = -FLIPPER_PWM_MAX;
+		}
+	}
+
+	FlipperPwm( JOINT_FR_FLIPPER, lFlipperPwmFR );
+	FlipperPwm( JOINT_FL_FLIPPER, lFlipperPwmFL );
+	FlipperPwm( JOINT_BR_FLIPPER, lFlipperPwmBR );
+	FlipperPwm( JOINT_BL_FLIPPER, lFlipperPwmBL );
+}
+
+void RTC_Spider2020_Crawler_Practice::FlipperPwm(int iJointIndex, long lPwm)
+{
+	double dKp					= 1000.0;
+	double dKd					= 10.0;
+	double dMotorRpm			= 0.0;
+	double dMotorVelocity		= 0.0;
+	double dFlipperTorque		= 0.0;
+	double q					= 0.0;
+	double dq					= 0.0;
+	double ref_dq				= 0.0;
+	double u					= 0.0;
+	Link* link = nullptr;
+
+	if((0 <= iJointIndex) && (iJointIndex < JOINT_NUM))
+	{
+		link = m_cnoid_Link[LINK_FR_FLIPPER + iJointIndex];
+	}
+	if(link == nullptr)
+	{
+		return;
+	}
+
+	// モータのトルク勾配からPWM指令値に対応するトルクと回転速度を求め、
+	// 回転速度から次の目標角度を計算して目標角度に対してPD制御する
+	// モータスペック: RE 35 diameter 35mm, 90 Watt, 24 volt
+	dMotorRpm			= (m_SUB_MOTOR_RPM/100.0)*labs(lPwm) - m_SUB_MOTOR_GRADIENT*m_SUB_MOTOR_TORQUE;
+	dMotorVelocity		= ((dMotorRpm/60.0)*m_cnoid_TimeStep)*(2.0*M_PI);
+	dFlipperTorque		= m_SUB_MOTOR_TORQUE*m_SUB_MOTOR_GEAR_RATIO;
+	q					= link->q();
+	dq					= link->dq();
+
+	if(labs(lPwm) <= FLIPPER_PWM_MIN)
+	{
+		// 現状維持
+	}
+	else
+	{
+		if(0 < lPwm)
+		{
+			m_cnoid_MotorAngle[iJointIndex] += dMotorVelocity;
+		}
+		else
+		{
+			m_cnoid_MotorAngle[iJointIndex] -= dMotorVelocity;
+		}
+	}
+
+	// 目標角度を求める
+	m_cnoid_RefAngle[iJointIndex] = m_cnoid_MotorAngle[iJointIndex]/m_SUB_MOTOR_GEAR_RATIO;
+
+	// 目標角度に対してPD制御する
+	u = (m_cnoid_RefAngle[iJointIndex] - q)*dKp + (ref_dq - dq)*dKd;
+	if(dFlipperTorque < fabs(u))
+	{
+		if(0.0 <= u)
+		{
+			u = dFlipperTorque;
+		}
+		else
+		{
+			u = -dFlipperTorque;
+		}
+	}
+
+	// リンクにトルクを設定する
+	link->u() = u;
+}
+
+
+extern "C"
+{
+	void RTC_Spider2020_Crawler_PracticeInit(RTC::Manager* manager)
+	{
+		coil::Properties profile(rtc_spider2020_crawler_practice_spec);
+		manager->registerFactory(profile, RTC::Create<RTC_Spider2020_Crawler_Practice>, RTC::Delete<RTC_Spider2020_Crawler_Practice>);
+	}
+};
