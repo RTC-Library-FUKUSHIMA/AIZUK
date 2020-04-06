43982c7f9b62ef9ef833b8a241e61acf009e66ef
Chiaki Saito <Saito.chiaki@aizuk.jp>
2020-04-06 10:20:28 +0900

    add RTC_Single_Jaco2_Preview to reproduce a movement of a Jaco2 of Spider2020 model

---
+++ Spider2020/Choreonoid/RTC_for_preview/RTC_Single_Jaco2_Preview/include/RTC_Single_Jaco2_Preview/RTC_Single_Jaco2_Preview.h
@@ -0,0 +1,319 @@
+﻿// -*- C++ -*-
+/*!
+ * @file  RTC_Single_Jaco2_Preview.h
+ * @brief Spider2020 コレオノイドモデルのJaco2アームプレビューRTC
+ * @date  $Date$
+ *
+ * $Id$
+ */
+
+#ifndef RTC_SINGLE_JACO2_PREVIEW_H
+#define RTC_SINGLE_JACO2_PREVIEW_H
+
+#include <cnoid/BodyIoRTC>
+#include <cnoid/EigenUtil>
+#include <rtm/idl/BasicDataTypeSkel.h>
+#include <rtm/idl/ExtendedDataTypesSkel.h>
+#include <rtm/idl/InterfaceDataTypesSkel.h>
+
+// Service implementation headers
+// <rtc-template block="service_impl_h">
+
+// </rtc-template>
+
+// Service Consumer stub headers
+// <rtc-template block="consumer_stub_h">
+#include "ExtendedDataTypesStub.h"
+#include "BasicDataTypeStub.h"
+
+// </rtc-template>
+
+#include <rtm/Manager.h>
+#include <rtm/DataFlowComponentBase.h>
+#include <rtm/CorbaPort.h>
+#include <rtm/DataInPort.h>
+#include <rtm/DataOutPort.h>
+
+#include "ArmAngularWrapper.h"
+#include "FingerStatusWrapper.h"
+
+using namespace cnoid;
+
+/*!
+ * @enum 列挙子
+ * リンクデータのインデクサ
+ */
+enum LINK_t
+{
+	LINK_SHOULDER,			/*!< SHOULDERリンク		*/
+	LINK_ARM,				/*!< ARMリンク			*/
+	LINK_FOREARM,			/*!< FOREARMリンク		*/
+	LINK_WRIST1,			/*!< WRIST1リンク		*/
+	LINK_WRIST2,			/*!< WRIST2リンク		*/
+	LINK_HAND,				/*!< HANDリンク			*/
+	LINK_FINGER1,			/*!< FINGER1リンク		*/
+	LINK_FINGER2,			/*!< FINGER2リンク		*/
+	LINK_FINGER3,			/*!< FINGER3リンク		*/
+	LINK_NUM,
+};
+
+/*!
+ * @class RTC_Single_Jaco2_Preview
+ * @brief Spider2020 コレオノイドモデルのJaco2アームプレビューRTC
+ *
+ */
+class RTC_Single_Jaco2_Preview
+  : public BodyIoRTC
+{
+public:
+	/*!
+	 * @brief constructor
+	 * @param manager Maneger Object
+	 */
+	RTC_Single_Jaco2_Preview(RTC::Manager* manager);
+
+	/*!
+	 * @brief destructor
+	 */
+	~RTC_Single_Jaco2_Preview();
+
+	// <rtc-template block="public_attribute">
+
+	// </rtc-template>
+
+	// <rtc-template block="public_operation">
+
+	// </rtc-template>
+
+	/***
+	 *
+	 * The initialize action (on CREATED->ALIVE transition)
+	 *
+	 * @return RTC::ReturnCode_t
+	 *
+	 *
+	 */
+	virtual RTC::ReturnCode_t onInitialize(Body* body) override;
+
+	/***
+	 *
+	 * The finalize action (on ALIVE->END transition)
+	 *
+	 * @return RTC::ReturnCode_t
+	 *
+	 *
+	 */
+	// virtual RTC::ReturnCode_t onFinalize();
+
+	/***
+	 *
+	 * The startup action when ExecutionContext startup
+	 *
+	 * @param ec_id target ExecutionContext Id
+	 *
+	 * @return RTC::ReturnCode_t
+	 *
+	 *
+	 */
+	// virtual RTC::ReturnCode_t onStartup(RTC::UniqueId ec_id);
+
+	/***
+	 *
+	 * The shutdown action when ExecutionContext stop
+	 *
+	 * @param ec_id target ExecutionContext Id
+	 *
+	 * @return RTC::ReturnCode_t
+	 *
+	 *
+	 */
+	// virtual RTC::ReturnCode_t onShutdown(RTC::UniqueId ec_id);
+
+	/***
+	 *
+	 * The activated action (Active state entry action)
+	 *
+	 * @param ec_id target ExecutionContext Id
+	 *
+	 * @return RTC::ReturnCode_t
+	 *
+	 *
+	 */
+	// virtual RTC::ReturnCode_t onActivated(RTC::UniqueId ec_id);
+
+	/***
+	 *
+	 * The deactivated action (Active state exit action)
+	 *
+	 * @param ec_id target ExecutionContext Id
+	 *
+	 * @return RTC::ReturnCode_t
+	 *
+	 *
+	 */
+	// virtual RTC::ReturnCode_t onDeactivated(RTC::UniqueId ec_id);
+
+	/***
+	 *
+	 * The execution action that is invoked periodically
+	 *
+	 * @param ec_id target ExecutionContext Id
+	 *
+	 * @return RTC::ReturnCode_t
+	 *
+	 *
+	 */
+	// virtual RTC::ReturnCode_t onExecute(RTC::UniqueId ec_id);
+
+	/***
+	 *
+	 * The aborting action when main logic error occurred.
+	 *
+	 * @param ec_id target ExecutionContext Id
+	 *
+	 * @return RTC::ReturnCode_t
+	 *
+	 *
+	 */
+	// virtual RTC::ReturnCode_t onAborting(RTC::UniqueId ec_id);
+
+	/***
+	 *
+	 * The error action in ERROR state
+	 *
+	 * @param ec_id target ExecutionContext Id
+	 *
+	 * @return RTC::ReturnCode_t
+	 *
+	 *
+	 */
+	// virtual RTC::ReturnCode_t onError(RTC::UniqueId ec_id);
+
+	/***
+	 *
+	 * The reset action that is invoked resetting
+	 *
+	 * @param ec_id target ExecutionContext Id
+	 *
+	 * @return RTC::ReturnCode_t
+	 *
+	 *
+	 */
+	// virtual RTC::ReturnCode_t onReset(RTC::UniqueId ec_id);
+
+	/***
+	 *
+	 * The state update action that is invoked after onExecute() action
+	 *
+	 * @param ec_id target ExecutionContext Id
+	 *
+	 * @return RTC::ReturnCode_t
+	 *
+	 *
+	 */
+	// virtual RTC::ReturnCode_t onStateUpdate(RTC::UniqueId ec_id);
+
+	/***
+	 *
+	 * The action that is invoked when execution context's rate is changed
+	 *
+	 * @param ec_id target ExecutionContext Id
+	 *
+	 * @return RTC::ReturnCode_t
+	 *
+	 *
+	 */
+	// virtual RTC::ReturnCode_t onRateChanged(RTC::UniqueId ec_id);
+
+	virtual bool initializeSimulation(ControllerIO* io) override;
+	virtual void inputFromSimulator() override;
+	virtual void outputToSimulator() override;
+	virtual void stopSimulation() override;
+
+protected:
+	// <rtc-template block="protected_attribute">
+
+	// </rtc-template>
+
+	// <rtc-template block="protected_operation">
+
+	// </rtc-template>
+
+	// Configuration variable declaration
+	// <rtc-template block="config_declare">
+
+	// </rtc-template>
+
+	// DataInPort declaration
+	// <rtc-template block="inport_declare">
+	RTC::TimedDoubleSeq m_Angular_In;
+	/*!
+	 * 腕の角度
+	 * - Type: TimedDoubleSeq
+	 * - Number: 6
+	 * - Semantics: [0]: 腕の1軸目
+	 *              [1]: 腕の2軸目
+	 *              [2]: 腕の3軸目
+	 *              [3]: 腕の4軸目
+	 *              [4]: 腕の5軸目
+	 *              [5]: 腕の6軸目
+	 * - Unit: [rad]
+	 */
+	RTC::InPort<RTC::TimedDoubleSeq> m_Angular_InIn;
+	RTC::TimedLongSeq m_Finger_In;
+	/*!
+	 * 指の開閉具合
+	 * - Type: TimedLongSeq
+	 * - Number: 3
+	 * - Semantics: [0]: 親指
+	 *              [1]: 差指
+	 *              [2]: 中指
+	 * - Unit: [u]
+	 */
+	RTC::InPort<RTC::TimedLongSeq> m_Finger_InIn;
+
+	// </rtc-template>
+
+	// DataOutPort declaration
+	// <rtc-template block="outport_declare">
+
+	// </rtc-template>
+
+	// CORBA Port declaration
+	// <rtc-template block="corbaport_declare">
+
+	// </rtc-template>
+
+	// Service declaration
+	// <rtc-template block="service_declare">
+
+	// </rtc-template>
+
+	// Consumer declaration
+	// <rtc-template block="consumer_declare">
+
+	// </rtc-template>
+
+private:
+	// <rtc-template block="private_attribute">
+
+	// </rtc-template>
+
+	// <rtc-template block="private_operation">
+
+	// </rtc-template>
+
+	ArmAngularWrapper m_Angular_InWrap;		/*!< m_Angular_Inのラッパークラス	*/
+	FingerStatusWrapper m_Finger_InWrap;	/*!< m_Finger_Inのラッパークラス	*/
+
+	Link* m_cnoid_Link[LINK_NUM];			/*!< コレオノイドモデルのリンク		*/
+
+};
+
+
+extern "C"
+{
+	DLL_EXPORT void RTC_Single_Jaco2_PreviewInit(RTC::Manager* manager);
+};
+
+#endif // RTC_SINGLE_JACO2_PREVIEW_H
---
+++ Spider2020/Choreonoid/RTC_for_preview/RTC_Single_Jaco2_Preview/src/RTC_Single_Jaco2_Preview.cpp
@@ -0,0 +1,260 @@
+﻿// -*- C++ -*-
+/*!
+ * @file  RTC_Single_Jaco2_Preview.cpp
+ * @brief Spider2020 コレオノイドモデルのJaco2アームプレビューRTC
+ * @date $Date$
+ *
+ * $Id$
+ */
+
+#include "RTC_Single_Jaco2_Preview.h"
+
+#define FINGER_ANGLE_MIN		(radian(0.0))
+#define FINGER_ANGLE_MAX		(radian(80.0))
+#define FINGER_UNIT_TO_ANGLE(x)	(radian((FINGER_ANGLE_MAX/6800.0)*(x)))
+#define FINGER_ANGLE_ADJUST(x)	\
+	((x) < FINGER_ANGLE_MIN) ? FINGER_ANGLE_MIN : \
+	((FINGER_ANGLE_MAX < (x)) ? FINGER_ANGLE_MAX : (x) )
+
+// Module specification
+// <rtc-template block="module_spec">
+static const char* rtc_single_jaco2_preview_spec[] =
+{
+	"implementation_id", "RTC_Single_Jaco2_Preview",
+	"type_name",         "RTC_Single_Jaco2_Preview",
+	"description",       "Spider2020 コレオノイドモデルのJaco2アームプレビューRTC",
+	"version",           "1.0.0",
+	"vendor",            "AIZUK",
+	"category",          "Controller",
+	"activity_type",     "PERIODIC",
+	"kind",              "DataFlowComponent",
+	"max_instance",      "1",
+	"language",          "C++",
+	"lang_type",         "compile",
+	""
+};
+// </rtc-template>
+
+/*!
+ * @brief constructor
+ * @param manager Maneger Object
+ */
+RTC_Single_Jaco2_Preview::RTC_Single_Jaco2_Preview(RTC::Manager* manager)
+	// <rtc-template block="initializer">
+	: BodyIoRTC(manager)
+	, m_Angular_InIn("Angular_In", m_Angular_In)
+	, m_Finger_InIn("Finger_In", m_Finger_In)
+
+	// </rtc-template>
+	, m_Angular_InWrap(m_Angular_In)
+	, m_Finger_InWrap(m_Finger_In)
+{
+}
+
+/*!
+ * @brief destructor
+ */
+RTC_Single_Jaco2_Preview::~RTC_Single_Jaco2_Preview()
+{
+}
+
+RTC::ReturnCode_t RTC_Single_Jaco2_Preview::onInitialize(Body* body)
+{
+	// Registration: InPort/OutPort/Service
+	// <rtc-template block="registration">
+	// Set InPort buffers
+	addInPort("Angular_In", m_Angular_InIn);
+	addInPort("Finger_In", m_Finger_InIn);
+
+	// Set OutPort buffer
+
+	// Set service provider to Ports
+
+	// Set service consumers to Ports
+
+	// Set CORBA Service Ports
+
+	// </rtc-template>
+
+	// <rtc-template block="bind_config">
+	// </rtc-template>
+
+	// 腕の角度の入力を初期化する
+	m_Angular_InWrap.InitData();
+
+	// 指の開閉具合の入力を初期化する
+	m_Finger_InWrap.InitData();
+
+	return RTC::RTC_OK;
+}
+
+/*
+RTC::ReturnCode_t RTC_Single_Jaco2_Preview::onFinalize()
+{
+	return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t RTC_Single_Jaco2_Preview::onStartup(RTC::UniqueId ec_id)
+{
+	return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t RTC_Single_Jaco2_Preview::onShutdown(RTC::UniqueId ec_id)
+{
+	return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t RTC_Single_Jaco2_Preview::onActivated(RTC::UniqueId ec_id)
+{
+	return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t RTC_Single_Jaco2_Preview::onDeactivated(RTC::UniqueId ec_id)
+{
+	return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t RTC_Single_Jaco2_Preview::onExecute(RTC::UniqueId ec_id)
+{
+	return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t RTC_Single_Jaco2_Preview::onAborting(RTC::UniqueId ec_id)
+{
+	return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t RTC_Single_Jaco2_Preview::onError(RTC::UniqueId ec_id)
+{
+	return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t RTC_Single_Jaco2_Preview::onReset(RTC::UniqueId ec_id)
+{
+	return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t RTC_Single_Jaco2_Preview::onStateUpdate(RTC::UniqueId ec_id)
+{
+	return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t RTC_Single_Jaco2_Preview::onRateChanged(RTC::UniqueId ec_id)
+{
+	return RTC::RTC_OK;
+}
+*/
+
+bool RTC_Single_Jaco2_Preview::initializeSimulation(ControllerIO* io)
+{
+	int i = 0;
+
+	if( io != NULL )
+	{
+		Body* body = io->body();
+		if( body != NULL )
+		{
+			// コレオノイドモデルのリンクを取得
+			m_cnoid_Link[LINK_SHOULDER]	= body->link( "SHOULDER" );
+			m_cnoid_Link[LINK_ARM]		= body->link( "ARM" );
+			m_cnoid_Link[LINK_FOREARM]	= body->link( "FOREARM" );
+			m_cnoid_Link[LINK_WRIST1]	= body->link( "WRIST1" );
+			m_cnoid_Link[LINK_WRIST2]	= body->link( "WRIST2" );
+			m_cnoid_Link[LINK_HAND]		= body->link( "HAND" );
+			m_cnoid_Link[LINK_FINGER1]	= body->link( "FINGER1" );
+			m_cnoid_Link[LINK_FINGER2]	= body->link( "FINGER2" );
+			m_cnoid_Link[LINK_FINGER3]	= body->link( "FINGER3" );
+			for( i = 0; i < LINK_NUM; i++ )
+			{
+				if( m_cnoid_Link[i] == NULL )
+				{
+					return false;
+				}
+			}
+
+			// 作動モードを設定
+			m_cnoid_Link[LINK_SHOULDER]->setActuationMode( Link::JOINT_ANGLE );
+			m_cnoid_Link[LINK_ARM]->setActuationMode( Link::JOINT_ANGLE );
+			m_cnoid_Link[LINK_FOREARM]->setActuationMode( Link::JOINT_ANGLE );
+			m_cnoid_Link[LINK_WRIST1]->setActuationMode( Link::JOINT_ANGLE );
+			m_cnoid_Link[LINK_WRIST2]->setActuationMode( Link::JOINT_ANGLE );
+			m_cnoid_Link[LINK_HAND]->setActuationMode( Link::JOINT_ANGLE );
+			m_cnoid_Link[LINK_FINGER1]->setActuationMode( Link::JOINT_ANGLE );
+			m_cnoid_Link[LINK_FINGER2]->setActuationMode( Link::JOINT_ANGLE );
+			m_cnoid_Link[LINK_FINGER3]->setActuationMode( Link::JOINT_ANGLE );
+		}
+	}
+
+	return true;
+}
+
+void RTC_Single_Jaco2_Preview::inputFromSimulator()
+{
+}
+
+void RTC_Single_Jaco2_Preview::outputToSimulator()
+{
+	// 腕の角度の入力を処理する
+	if(m_Angular_InIn.isNew())
+	{
+		// 次のデータを読み込む
+		m_Angular_InIn.read();
+
+		// 腕の各リンクに角度を設定する
+		m_cnoid_Link[LINK_SHOULDER]->q_target() = m_Angular_InWrap.GetShoulderAngular();
+		m_cnoid_Link[LINK_ARM]->q_target()		= m_Angular_InWrap.GetArmAngular();
+		m_cnoid_Link[LINK_FOREARM]->q_target()	= m_Angular_InWrap.GetForearmAngular();
+		m_cnoid_Link[LINK_WRIST1]->q_target()	= m_Angular_InWrap.GetWrist1Angular();
+		m_cnoid_Link[LINK_WRIST2]->q_target()	= m_Angular_InWrap.GetWrist2Angular();
+		m_cnoid_Link[LINK_HAND]->q_target()	 	= m_Angular_InWrap.GetHandAngular();
+	}
+
+	// 指の開閉具合の入力を処理する
+	if(m_Finger_InIn.isNew())
+	{
+		// 次のデータを読み込む
+		m_Finger_InIn.read();
+
+		// 指の各リンクに角度を設定する
+		m_cnoid_Link[LINK_FINGER1]->q_target() = FINGER_ANGLE_ADJUST(FINGER_UNIT_TO_ANGLE(m_Finger_InWrap.GetThumbStatus()));
+		m_cnoid_Link[LINK_FINGER2]->q_target() = FINGER_ANGLE_ADJUST(FINGER_UNIT_TO_ANGLE(m_Finger_InWrap.GetIndexFingerStatus()));
+		m_cnoid_Link[LINK_FINGER3]->q_target() = FINGER_ANGLE_ADJUST(FINGER_UNIT_TO_ANGLE(m_Finger_InWrap.GetMiddleFingerStatus()));
+	}
+}
+
+void RTC_Single_Jaco2_Preview::stopSimulation()
+{
+}
+
+
+extern "C"
+{
+
+	void RTC_Single_Jaco2_PreviewInit(RTC::Manager* manager)
+	{
+		coil::Properties profile(rtc_single_jaco2_preview_spec);
+		manager->registerFactory(
+			profile, RTC::Create<RTC_Single_Jaco2_Preview>, RTC::Delete<RTC_Single_Jaco2_Preview>);
+	}
+
+};
